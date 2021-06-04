c91a068c16c19ce5faa811a58e592f94eae7e751
Marko <mbudisic@clarkson.edu>
2021-06-03 23:31:19 -0400

    Added SVD Lapack routine from MATLAB

---
+++ synthetic_demo/test_svd.m
@@ -0,0 +1,11 @@
+[U,S,V] = svd_lapack(X,0,'gesvd');
+plot(diag(S))
+semilogy(diag(S),'o','DisplayName','LAPACK gesvd');
+hold on;
+[U,S2,V] = svd(X,0);
+hold on
+semilogy(diag(S2),'x','DisplayName','MATLAB SVD0 full')
+S3 = svd(X,0);
+semilogy(S3,'s','DisplayName','MATLAB SVD0 values')
+[U,S4,V] = svd_lapack(X,0,'gesdd');
+semilogy(diag(S4),'*','DisplayName','LAPACK gesdd')
