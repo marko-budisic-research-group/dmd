ad075cf79f8e8f9689184ef891fc6b9e07b4cdcd
Marko <mbudisic@clarkson.edu>
2021-06-23 13:58:52 -0400

    Corrected the normalization step

--- dmd.m
+++ dmd.m
@@ -41,7 +41,7 @@ function out = dmd( DataMatrix, dt, rom_dim, varargin )
 %         If VALUE = -1, all snapshots are used.
 %         If VALUE = [], the code does not compute b coefficients but
 %                        exits as soon as eigenvalues/modes are computed.
-%         Default: VALUE = 1 (fit at the initial condition) 
+%         Default: VALUE = 1 (fit at the initial condition)
 %
 %    out = dmd( DataMatrix, dt, rom_dim, 'numericalRankTolerance', VALUE)
 %         VALUE is the threshold for ratios sigma_n/sigma_1 of singular values
@@ -142,47 +142,47 @@ X2=DataMatrix(:,2:end);
 %% Mean/frequency removal
 if ~isempty(options.removefrequencies)
     disp('Removing predetermined frequencies')
-    
+
     % row vector of continuous frequencies
     OmegaR = reshape( options.removefrequencies, 1, [] );
-    
+
     % discrete frequencies row vector
     LambdaR = exp( OmegaR * dt );
-    
+
     % Vandermonde
     Vandermonde = LambdaR .^ transpose( 0:( Nsnapshots-1 ) );
-    
+
     % truncated vandermonde (for dealing with X1 and X2)
     VR_o = Vandermonde(2:end,:);
-    
+
     % projectors
     PI = pinv(transpose(Vandermonde))*transpose(Vandermonde);
     PI_o = pinv(transpose(VR_o))*transpose(VR_o);
-    
+
     X1 = X1 - X1*PI_o;
     X2 = X2 - X2*PI_o;
-    
-    
+
+
     % the following two calculations are in-principle equivalent, but not
     % exactly when number of time steps is finite
     HarmonicAverage = DataMatrix*pinv(transpose(Vandermonde));
     %HarmonicAverage = DataMatrix*conj(Vandermonde)/Nsnapshots;
-    
-    
+
+
     [out.AvgPhi, out.AvgB] = normalizeModes(HarmonicAverage);
-    
+
     out.AvgOmega = options.removefrequencies(:);
     out.AvgLambda = LambdaR(:);
     out.AvgB = out.AvgB(:);
-    
+
     meanL2 = abs(out.AvgB) .* sqrt( (exp(2*real(out.AvgOmega)*T)-1)./(2*real(out.AvgOmega)*T) );
     meanL2(isnan(meanL2)) = abs(out.AvgB(isnan(meanL2)));
-    
+
     out.AvgMeanL2norm = meanL2;
-    
+
     clear PI_o;
     clear PI;
-    
+
 end
 
 %% Subselect pairs
@@ -193,8 +193,9 @@ end
 
 % pre-normalization
 if options.normalize
-    X1 = normalize(X1, 'norm',2);
-    X2 = normalize(X2, 'norm',2);
+    X1norms = vecnorm(X1,2);
+    X1 = X1./X1norms;
+    X2 = X2./X1norms;
 end
 
 % total-DMD Hemati debias
@@ -233,14 +234,14 @@ switch(options.dmd_type)
         disp('Exact DMD');
         % Build Atilde
         singular_values = diag(Sigmar);
-        
+
         Atilde = transpose(Ur)*X2*Vr*diag(1./singular_values);
-        
+
         assert( all(~isnan(Atilde),'all') && all(~isinf(Atilde),'all'), ...
             "Atilde shouldn't contain NaN or Inf - check invertibility of Sr")
-        
+
         assert( length(Atilde) == rom_dim, "Requested model rank not achieved")
-        
+
         % Compute DMD Modes
         [W, Lambda] = eig(Atilde);
         Phi = X2*Vr*diag(1./singular_values)*W;
@@ -248,32 +249,32 @@ switch(options.dmd_type)
         %% Compute continuous-time eigenvalues
         lambda = diag(Lambda);
         optimalResiduals = nan(size(lambda));
-        
+
     case 'rrr'
         disp('DDMD_RRR');
-        
+
         % Algorithm 2 from:
         % Drmač, Zlatko, Igor Mezić, and Ryan Mohr. 2018.
         % “Data Driven Modal Decompositions: Analysis and Enhancements.”
         % SIAM Journal on Scientific Computing 40 (4): A2253–85. https://doi.org/10.1137/17M1144155.
-        
-        
+
+
         AUr = X2*(Vr*diag(1./sigma(1:subspaceSize)));
         clear X2;
-        
+
         R = triu( qr( [Ur, AUr], 0 ) );
         numRankPrime = min( size(Ur,1) - subspaceSize, subspaceSize );
         Rblocks = mat2cell( R, [subspaceSize, size(Ur,1)-subspaceSize], [subspaceSize, subspaceSize] );
         [R11,~,R12,R22] = deal(Rblocks{:});
-        
+
         % Rayleigh quotient
         Sk = diag( conj( diag(R11) ) )*R12;
         ritz = eig(Sk); % Ritz values - can be used as DMD values
-        
+
         W = nan(subspaceSize);
         optimalResiduals = nan(subspaceSize,1);
-        
-        
+
+
         %%
         % Ritz value adjustment loop
         %
@@ -283,8 +284,8 @@ switch(options.dmd_type)
         ritzes = ritz;
         Mleft = [R12; R22];
         Mright = [R11; zeros(size(R22))];
-        
-        
+
+
         while not( allclose(ritzOld, ritz, options.ritzATOL, options.ritzRTOL) ) && ...
                 count < options.ritzMaxIteration
             count = count+1;
@@ -292,13 +293,13 @@ switch(options.dmd_type)
             disp("Ritz correction try " + count + " Discrepancy = " + discrepancy(count) );
             ritzOld = ritz;
             for i = 1:subspaceSize
-                
+
                 M = Mleft - ritz(i)*Mright;
-                
+
                 % compute smallest singular value
                 [~,SS,VV] = mysvd( M );
                 optimalResiduals(i) = SS(end,end);
-                
+
                 svec_min = VV(:,end);
                 ritz(i) = svec_min' * Sk * svec_min;
                 W(:,i) = svec_min;
@@ -309,7 +310,7 @@ switch(options.dmd_type)
         %%
         out.discrepancy = discrepancy;
         out.ritzes = ritzes;
-        
+
         Phi = Ur*W;
         lambda = ritzes(:,1);
 end
@@ -328,61 +329,61 @@ out.model_rank = length(lambda);
 %% compute combination coefficients
 % by L2-fit to a sequence of snapshots
 if ~isempty(options.step)
-    
+
     if options.step == -1
         options.step = 1:size(DataMatrix,2);
     end
-    
-    
+
+
     disp("Computing b by L2 fit at snapshots " + ...
         num2str(options.step, "%d") );
     disp( "time = " + ...
         num2str( (options.step-1)*dt, "%.2f ") );
-    
+
     % LHS: modes advanced to required steps
     LHS = zeros([size(Phi), numel(options.step)]);
-    
+
     % advance mode vectors by steps and set them into layers of LHS tensor
     for k = 1:numel(options.step)
         LHS(:,:,k) = Phi*Lambda^(options.step(k)-1);
     end
-    
+
     % reshape LHS into a 2D matrix so that the layers are stacked on top of
     % each other
     LHS = permute(LHS, [1,3,2]);
     LHS = reshape(LHS,[], size(Phi,2) );
-    
+
     % basically
     % LHS = [ Phi * D^0 ; Phi * D^1 ; ...]
-    
-    
+
+
     % RHS: corresponding snapshots
     RHS = DataMatrix(:, options.step);
     RHS = reshape(RHS,[], 1);
-    
+
     % basically
     % RHS = [Datamatrix(:,1); Datamatrix(:,2);, ...]
-    
+
     % not always equivalent to LHS\RHS
     % see: https://www.mathworks.com/help/matlab/ref/lsqminnorm.html#mw_e9cfa6e3-ccc3-4830-802c-df496b9e452b
     b = lsqminnorm(LHS, RHS);
-    
-    
+
+
     %% Compute mean L2 contribution of each mode
     meanL2norm = abs(b) .* sqrt( (exp(2*real(omega)*T)-1)./(2*real(omega)*T) );
     meanL2norm(isnan(meanL2norm)) = abs(b(isnan(meanL2norm))); % L2 norm of non-exponentials
-    
+
     % when real(omega) is zero, the formula above doesn't work as magnitude of
     % the mode is constant - so we compute it manually
     idx_small = abs(real(omega)*T) < 1e-12;
     meanL2norm(idx_small) = abs(b(idx_small));
-    
+
     % catch any remaining errors
     assert(all( ~isnan(meanL2norm) ), "Problems: L2 norm computation failed");
-    
-    
+
+
     %% Sorting
-    
+
     switch(options.sortby)
         case 'initcond'
             [out.ranking,idx] = sort( abs(b), 'descend' );
@@ -393,8 +394,8 @@ if ~isempty(options.step)
         otherwise
             error('Unknown sorting option')
     end
-    
-    
+
+
     out.optimalResiduals = optimalResiduals(idx);
     out.meanL2norm = meanL2norm(idx);
     out.b = b(idx);
@@ -402,7 +403,7 @@ if ~isempty(options.step)
     out.omega = omega(idx);
     out.lambda = lambda(idx);
     out.model_rank = length(lambda);
-    
+
 end
 
 end
